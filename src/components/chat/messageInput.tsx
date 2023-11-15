9217442272252e5ce9379de2c723fd82a4056ecc
katongole isaac <katongolelsaac78@gmail.com>
2023-11-15 20:00:05 +0300

    message input fix

--- src/components/chat/messageInput.tsx
+++ src/components/chat/messageInput.tsx
@@ -15,22 +15,13 @@ interface MsgProps {
 }
 
 const MessageInput = ({ onSubmit, chatDivRef }: MsgProps) => {
+
   const [message, setMessage] = useState("");
-  const msgInputRef = useRef<HTMLTextAreaElement>(null);
+  const msgInputRef = useRef<HTMLDivElement>(null);
+  const placeholder = "Type your message here...";
 
   const [readyToSend, setReadyToSend] = useState(false);
 
-  const handleOnChange = ({
-    target,
-  }: React.ChangeEvent<HTMLTextAreaElement>) => {
-    // check if there is atleast one character in the message
-    if (/[\w\S]+/g.test(target.value)) setReadyToSend(true);
-    else setReadyToSend(false);
-
-    setMessage(target.value);
-
-    console.log(message);
-  };
 
   const handleKeyPress = (e: KeyboardEvent) => {
     // on press Enter key
@@ -41,7 +32,7 @@ const MessageInput = ({ onSubmit, chatDivRef }: MsgProps) => {
     ) {
       e.preventDefault();
       onSubmit(message.trim());
-      msgInputRef.current!.value = "";
+      // msgInputRef.current!.value = "";
       setMessage("");
       setReadyToSend(false);
 
@@ -56,53 +47,70 @@ const MessageInput = ({ onSubmit, chatDivRef }: MsgProps) => {
       e.code === "Enter" &&
       document.activeElement === msgInputRef.current
     )
-      msgInputRef.current!.rows += 1;
+      // msgInputRef.current!.rows += 1;
 
     // there are multi line without text, clear them and reset to rows =2
-    if (e.code === "Enter" && !message && msgInputRef.current!.rows > 2) {
+    // if (e.code === "Enter" && !message && msgInputRef.current!.rows > 2) {
       e.preventDefault();
-      msgInputRef.current!.rows = 2;
-    }
+      // msgInputRef.current!.rows = 2;
+    // }
   };
 
   const handleSendClick = (msg: string) => {
     if (!(readyToSend && message)) return;
 
     onSubmit(msg.trim());
-    msgInputRef.current!.value = "";
+    // msgInputRef.current!.value = "";
     setMessage("");
     setReadyToSend(false);
   };
 
+  const handlePaste = function (event: React.ClipboardEvent) {
+    event.preventDefault(); // Prevent default paste behavior
+
+    // Get the plain text content from the clipboard
+    const pastedText = event.clipboardData!.getData("text/plain");
+    setMessage(pastedText);
+  }
+
   useEffect(() => {
     document.addEventListener("keypress", handleKeyPress);
-
+    
     return () => {
       document.removeEventListener("keypress", handleKeyPress);
+
     };
   });
 
-  useEffect(() => {
-    chatDivRef.current.scrollTop = chatDivRef.current.scrollHeight;
-  }, [message]);
+
 
   return (
     <div className=" w-full py-2 px-2  space-x-2  bg-gray-200 dark:bg-neutral-700 ">
       <div className="w-full flex items-center justify-center gap-4">
-        <div className="flex w-full items-center gap-2 px-3 bg-neutral-100 dark:bg-[#343434] ">
-          {/*  message input */}
-          <ChatActions />
-          <div className="w-full flex-1 h-full max-h-32 overflow-y-auto custom-scrollbar ">
-            <textarea
-              className=" px-3 bg-transparent text-slate-700 dark:text-gray-100 rounded-md w-full focus:outline-none resize-none custom-scrollbar"
-              placeholder="Write a message"
-              value={message}
-              ref={msgInputRef}
-              onChange={(e) => handleOnChange(e)}
-            />
-          </div>
+        <ChatActions />
+        <div className="flex w-full items-center gap-2 px-3 bg-neutral-100 dark:bg-[#343434] rounded-md ">
+            {/* emoji picker */}
+            <div className="self-end pb-3 ">
+            <EmojiPicker onEmojiSelect={setMessage}  />
+            </div>
 
-          <EmojiPicker onEmojiSelect={setMessage} />
+          <div className="w-full  flex-1 h-full max-h-28 overflow-y-auto custom-scrollbar ">
+           
+            {/* message input */}
+            <div
+              className="w-full min-h-[2.5em] max-h-[7.4em] py-2 px-2 placeholder:text-gray-500 focus:outline-none"
+              placeholder="write a message"
+              title="Type a message"
+              id="message-input"
+              onPaste={handlePaste}
+              ref={msgInputRef}
+              data-placeholder={placeholder}
+              spellCheck
+              contentEditable
+            >
+              {message}
+            </div>
+          </div>
         </div>
 
         <button
